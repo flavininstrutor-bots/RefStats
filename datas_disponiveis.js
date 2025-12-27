e4bef69ea1d074cfedbf0c230c2a7bd5e885e8a1
flavininstrutor-bots <flavininstrutor@gmail.com>
2025-12-27 00:11:26 -0300

    AtualizacaoHistorico

--- datas_disponiveis.js
+++ datas_disponiveis.js
@@ -1,5 +1,5 @@
 // Arquivo gerado automaticamente por gerar_datas_historico.py
-// Atualizado em: 26/12/2025 22:50:02
-// Total de datas: 6
+// Atualizado em: 27/12/2025 00:11:05
+// Total de datas: 7
 
-const DATAS_DISPONIVEIS = ["01122025", "02122025", "03122025", "04122025", "05122025", "10122025"];
+const DATAS_DISPONIVEIS = ["01122025", "02122025", "03122025", "04122025", "05122025", "10122025", "11122025"];
