96479ed416f094371462b5150951aef2645dd4fa
flavininstrutor-bots <flavininstrutor@gmail.com>
2025-12-26 21:41:25 -0300

    Base RefStats

---
+++ datas_disponiveis.js
@@ -0,0 +1,5 @@
+// Arquivo gerado automaticamente por gerar_datas_historico.py
+// Atualizado em: 26/12/2025 20:53:26
+// Total de datas: 5
+
+const DATAS_DISPONIVEIS = ["02122025", "03122025", "04122025", "05122025", "10122025"];
