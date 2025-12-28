24ef41bb8715b156262858d38a1a0bac1b001280
flavininstrutor-bots <flavininstrutor@gmail.com>
2025-12-28 00:46:07 -0300

    JogosDoDia

--- datas_disponiveis.js
+++ datas_disponiveis.js
@@ -1,5 +1,5 @@
 // Arquivo gerado automaticamente por gerar_datas_historico.py
-// Atualizado em: 27/12/2025 00:11:05
-// Total de datas: 7
+// Atualizado em: 28/12/2025 00:45:34
+// Total de datas: 11
 
-const DATAS_DISPONIVEIS = ["01122025", "02122025", "03122025", "04122025", "05122025", "10122025", "11122025"];
+const DATAS_DISPONIVEIS = ["01122025", "02122025", "03122025", "04122025", "05122025", "06122025", "07122025", "08122025", "10122025", "11122025", "28122025"];
